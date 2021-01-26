dc619805b258a00a9d9c62df191957738889e1c9
Егор Домнин <edomnin@ml-linx-2.dev.ivi.ru>
2021-01-26 17:00:11 +0300

    Initial commit

---
+++ project_folder/src/base.py
@@ -0,0 +1,9 @@
+"""."""
+
+
+class BaseClass:
+    """."""
+
+    def __init__(self) -> None:
+        """."""
+        pass
---
+++ project_folder/test.py
@@ -0,0 +1,11 @@
+# Standard Library
+import json
+
+# Third Party
+from tqdm import tqdm
+
+# Our Projects
+from our_project import AnotherClass
+
+# First Party
+from src.base import BaseClass
